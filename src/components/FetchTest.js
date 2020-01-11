e1b256b8d79ae5b2a0fed0c515b2ede942e60676
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2020-01-11 01:44:14 +0100

    useReducer + somes tricks

--- src/components/FetchTest.js
+++ src/components/FetchTest.js
@@ -1,10 +1,33 @@
 import React, { useEffect, useReducer, useState } from "react";
 import { localisation, API_TOKEN } from "../normally_from_back";
 
-const URL = `https://api.netatmo.com/api/getpublicdata?lat_ne=${localisation.paris.lat_ne}&lon_ne=${localisation.paris.lon_ne}&lat_sw=${localisation.paris.lat_sw}&lon_sw=${localisation.paris.lon_sw}`;
+const initialState = {
+  temperature: "",
+  humidity: ""
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TEMPERATURE":
+      return {
+        ...state,
+        temperature: action.measure
+      };
+    case "HUMIDITY":
+      return {
+        ...state,
+        humidity: action.measure
+      };
+    default:
+      return state;
+  }
+};
 
 function FetchTest() {
   const [city, setCity] = useState("paris");
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const URL = `https://api.netatmo.com/api/getpublicdata?lat_ne=${localisation[city].lat_ne}&lon_ne=${localisation[city].lon_ne}&lat_sw=${localisation[city].lat_sw}&lon_sw=${localisation[city].lon_sw}`;
 
   const getMeasures = (fetched, val) => {
     const reports = fetched.body.map(obj => {
@@ -18,6 +41,11 @@ function FetchTest() {
     const total = reports.reduce((acc, val) => acc + val);
     const average = total / reports.length;
     console.log(`${val === 0 ? "temperature" : "humidity"} : `, average);
+
+    dispatch({
+      type: val === 0 ? "TEMPERATURE" : "HUMIDITY",
+      measure: average
+    });
   };
 
   useEffect(() => {
@@ -40,13 +68,44 @@ function FetchTest() {
         console.log("pas pour cette fois");
       }
     };
-
     fetchData();
-  }, []);
+  }, [URL]);
 
-  console.log(localisation);
+  const updateCity = e => {
+    setCity(e.target.name);
+    console.log("city updated");
+  };
 
-  return <div>yoyo</div>;
+  console.log("state : ", state);
+
+  const { temperature, humidity } = state;
+
+  return (
+    <div>
+      <button onClick={updateCity} name="paris">
+        Paris
+      </button>
+
+      <button onClick={updateCity} name="new_york">
+        New York
+      </button>
+
+      <button onClick={updateCity} name="berlin">
+        Berlin
+      </button>
+
+      <button onClick={updateCity} name="bogota">
+        Bogota
+      </button>
+
+      <div>
+        <h2>Temperature</h2>
+        {temperature}
+        <h2>Humidity</h2>
+        {humidity}
+      </div>
+    </div>
+  );
 }
 
 export default FetchTest;
